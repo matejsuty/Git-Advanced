03d1db91239380bfefa627b03af833be71cb1438
vozar.david <vozar.david8@gmail.com>
2022-12-05 16:27:49 +0100

    git advanced intro

--- Member B/app.js
+++ Member B/app.js
@@ -2,12 +2,7 @@
 
 
 function count(age) {
-<<<<<<< HEAD
     console.log(`Countinggg the countless ${age}`);
-=======
-    console.log(`Counting the countless ${age}`);
-
->>>>>>> d59ef2af332dbaff13f1b2c13caea24691e95840
 }
 
-console.log(count(26,'dusan'));
+console.log(count(26, 'dusan'));
